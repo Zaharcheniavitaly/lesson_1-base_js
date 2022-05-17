40488bed0fabe127ef03ed8a21b747762a70d756
Zaharcheniavitaly <zaharchenyavitaly@yandex.by>
2022-05-17 20:42:18 +0300

    Update script.js

---
+++ css/js/script.js
@@ -0,0 +1,21 @@
+
+"use strict";
+
+
+let temperatureCels = prompt("Введите температуру в градусах по Цельсию");
+
+let temperatureFahr = (9 / 5) * temperatureCels + 32;
+
+alert(temperatureFahr);
+
+
+
+
+
+
+
+let name = "Василий";
+
+let admin = name;
+
+alert(admin);
